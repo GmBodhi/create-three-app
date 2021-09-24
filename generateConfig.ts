2bfa759504c4dff71216b3147e36eee2c8f12a76
All Things Cursed Known to Mankind <91156474+cursedstuff@users.noreply.github.com>
2021-09-24 10:40:06 -0500

    Update generateConfig.ts

--- generateConfig.ts
+++ generateConfig.ts
@@ -2,22 +2,27 @@ import fs from "fs";
 import path from "path";
 
 // DON'T EDIT OR DELETE THIS FILE. //
-let config = {};
-function manageDir(directory, json, target = "") {
+const config: {
+  [dir: string]: { files: { [file: string]: string; };; dirs: string[]; };
+} = {};
+
+// ! Consider using `path.join` to join file paths so it is cross-platform...
+
+function manageDir(directory: string, json: { dirs: string[]; files: { [file: string]: string; }; }, target = "") {
   fs.readdirSync(directory, { withFileTypes: true }).forEach((file) => {
     if (file.isDirectory()) {
       manageDir(
         path.join(directory, file.name),
         json,
-        target + "/" + file.name
+        target + "/" + file.name,
       );
 
       if (!json["dirs"].includes(target + "/" + file.name))
         json["dirs"].unshift(target + "/" + file.name);
     } else {
-      let dir = path.relative(
+      const dir = path.relative(
         path.join(__dirname, "examples"),
-        path.join(directory)
+        path.join(directory),
       );
       
       json["files"][file.name] = dir.split("\\").slice(1).join("/"); // why split by backslash?
@@ -27,27 +32,32 @@ function manageDir(directory, json, target = "") {
 
 fs.readdirSync(path.join(__dirname, "examples"), {
   withFileTypes: true,
-}).forEach(function (file) {
+}).forEach((file) => {
   if (file.isDirectory()) {
     config[file.name] = { files: {}, dirs: [] };
+    
     return manageDir(
       path.join(__dirname, "examples", file.name),
       config[file.name],
-      ""
+      "",
     );
   }
   return;
 });
+
 saveFile(config);
+
 function saveFile(json) {
-  let file = Object.fromEntries(
-    Object.entries(json).filter((val) => {
+  const file = Object.fromEntries(
+    Object.entries(json).map((val) => {
       val[1].dirs = val[1].dirs.sort(
         (a, b) => a.split("/").length - b.split("/").length
       );
-      return true;
-    })
+      
+      return val;
+    });
   );
+  
   fs.writeFileSync("./examples/config.json", JSON.stringify(file));
 }
 // END OF FILE //
