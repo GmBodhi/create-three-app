b76e90b5125286ca85728df461bd4a6f5fb190bc
GmBodhi <zakuzzdj@gmail.com>
2021-10-02 02:12:14 +0000

    fix

--- example-processor/index.js
+++ example-processor/index.js
@@ -9,31 +9,30 @@ function writeAssets(json) {
     writeFileSync(`./templates/${key}/assets.json`, JSON.stringify(value));
   });
 }
+initServer().then((port) => {
+  const init = () => {
+    mkdirSync("./templates");
+    fetch(`http://localhost:${port}/examples/files.json`)
+      .then((r) => r.json())
+      .then(async (json) => {
+        let targets = [];
+        Object.values(json).forEach((r) => targets.push(...r));
+        let urls = {};
+        await fetch1.launch({ urls, json: targets, port });
+        for (let key of targets) {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          await fetch1.fetch(
+            `http://localhost:${port}/examples/${key}.html`,
+            key
+          );
+        }
+        writeFileSync("./templates/assets.json", JSON.stringify(urls));
+        writeAssets(urls);
+        await fetch1.close();
+      });
+  };
 
-const port = initServer();
-
-const init = () => {
-  mkdirSync("./templates");
-  fetch(`http://localhost:${port}/examples/files.json`)
-    .then((r) => r.json())
-    .then(async (json) => {
-      let targets = [];
-      Object.values(json).forEach((r) => targets.push(...r));
-      let urls = {};
-      await fetch1.launch({ urls, json: targets, port });
-      for (let key of targets) {
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        await fetch1.fetch(
-          `http://localhost:${port}/examples/${key}.html`,
-          key
-        );
-      }
-      writeFileSync("./templates/assets.json", JSON.stringify(urls));
-      writeAssets(urls);
-      await fetch1.close();
-    });
-};
-
-if (existsSync("./templates")) {
-  rimraf("./templates", init);
-} else init();
+  if (existsSync("./templates")) {
+    rimraf("./templates", init);
+  } else init();
+});
--- example-processor/initServer.js
+++ example-processor/initServer.js
@@ -1,17 +1,21 @@
 const http = require("http");
 const handler = require("serve-handler");
 const { getPort } = require("portfinder-sync");
-const { sync } = require("cross-spawn")
+const  sync  = require("cross-spawn")
 
 module.exports.init = function init() {
   const port = getPort(2000);
 
-  sync("git clone --depth=1 https://github.com/mrdoob/three.js.git res");
+  return new Promise((resolve) => {
+    sync("git clone --depth=1 https://github.com/mrdoob/three.js.git res").on("exit", () => {
 
-  const server = http.createServer((req, res) =>
-    handler(req, res, { cleanUrls: false, public: "/res" })
-  );
-  server.listen(port);
-
-  return port;
+      
+      const server = http.createServer((req, res) =>
+      handler(req, res, { cleanUrls: false, public: "/res" })
+      );
+      server.listen(port);
+      
+      resolve(port)
+    })
+  });
 };
