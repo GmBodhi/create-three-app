7d72094311d7b4dad63b63c064cde5039182d6a9
GmBodhi <zakuzzdj@gmail.com>
2021-10-02 01:48:05 +0000

    test

--- example-processor/initServer.js
+++ example-processor/initServer.js
@@ -1,12 +1,15 @@
 const http = require("http");
 const handler = require("serve-handler");
 const { getPort } = require("portfinder-sync");
+const { sync } = require("cross-spawn")
 
 module.exports.init = function init() {
   const port = getPort(2000);
 
+  sync("git clone --depth=1 https://github.com/mrdoob/three.js.git res");
+
   const server = http.createServer((req, res) =>
-    handler(req, res, { cleanUrls: false })
+    handler(req, res, { cleanUrls: false, public: "/res" })
   );
   server.listen(port);
 
