31131e4e7c02a9179007dd2be14285a1999ae057
GmBodhi <zakuzzdj@gmail.com>
2021-10-02 02:34:18 +0000

    debug: 003

--- example-processor/initServer.js
+++ example-processor/initServer.js
@@ -1,22 +1,22 @@
 const http = require("http");
 const handler = require("serve-handler");
 const { getPort } = require("portfinder-sync");
-const  sync  = require("cross-spawn");
 const { default: fetch } = require("node-fetch");
 
 module.exports.init = function init() {
   const port = getPort(2000);
 
   return new Promise((resolve) => {
-    sync("git", ["clone", "--depth=1", "https://github.com/mrdoob/three.js.git", "res"]).on("exit", () => {
-
-      
-      const server = http.createServer((req, res) =>
-      handler(req, res, { cleanUrls: false, public: require("path").resolve(process.cwd(),"/res") })
-      );
-      server.listen(port);
-      fetch("http://localhost:2000/exaples/files.json").then(res => res.text()).then(console.log)
-      resolve(port)
-    })
+    const server = http.createServer((req, res) =>
+      handler(req, res, {
+        cleanUrls: false,
+        public: require("path").resolve(__dirname, "../res"),
+      })
+    );
+    server.listen(port);
+    fetch("http://localhost:2000/exaples/files.json")
+      .then((res) => res.text())
+      .then(console.log);
+    resolve(port);
   });
 };
