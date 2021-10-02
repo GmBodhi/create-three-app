036f0ec1e7f6cca86d5215cc641cf71526000847
GmBodhi <zakuzzdj@gmail.com>
2021-10-02 02:15:27 +0000

    fix

--- example-processor/initServer.js
+++ example-processor/initServer.js
@@ -7,7 +7,7 @@ module.exports.init = function init() {
   const port = getPort(2000);
 
   return new Promise((resolve) => {
-    sync("git clone --depth=1 https://github.com/mrdoob/three.js.git res").on("exit", () => {
+    sync("git", ["clone", "--depth=1", "https://github.com/mrdoob/three.js.git", "res"]).on("exit", () => {
 
       
       const server = http.createServer((req, res) =>
