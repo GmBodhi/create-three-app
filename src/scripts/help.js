d20511ba02845e202bd4d69b72a0fbd1a66fc5b0
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-10-21 17:14:46 +0530

    style: ignore formating for help menu

--- src/scripts/help.js
+++ src/scripts/help.js
@@ -1,47 +1,24 @@
-const chalk = require("chalk");
+const { blueBright, magentaBright, greenBright } = require("chalk");
 
 module.exports.help = () => {
+  // prettier-ignore
   process.stdout.write(`
-    Usage: npx create-three-app ${
-      chalk.blueBright("[options]") + chalk.magentaBright(" {directory}")
-    }
+    Usage: npx create-three-app ${blueBright("[options]") + magentaBright(" {directory}")}
 
     Options:
-        ${chalk.blueBright("-h")}, ${chalk.blueBright(
-    "--help"
-  )}                  Print this help message
-        ${chalk.blueBright("-v")}, ${chalk.blueBright(
-    "--version"
-  )}               Print the version number
-        ${chalk.blueBright("-t")}, ${chalk.blueBright(
-    "--template"
-  )} ${chalk.magentaBright("{name}")}       Specify the name of the template
-        ${chalk.blueBright("-e")}, ${chalk.blueBright(
-    "--example"
-  )}               Select templates from three.js examples
-        ${chalk.blueBright("-f")}, ${chalk.blueBright(
-    "--force"
-  )}                 This will delete all the contents of ${chalk.magentaBright(
-    "{directory}"
-  )}
-        ${chalk.blueBright("-b")}, ${chalk.blueBright(
-    "--bundler" +chalk.magentaBright(" {name}")
-  )}        Select the bundler to use ${chalk.blueBright(
-    "[webpack|parcel]"
-  )} (looking for a contributer)
-        ${chalk.blueBright(
-          "--prefer-npm"
-        )}                This will prefer npm when installing dependencies
+        ${blueBright("-h")}, ${blueBright("--help")}                  Print this help message
+        ${blueBright("-v")}, ${blueBright("--version")}               Print the version number
+        ${blueBright("-t")}, ${blueBright("--template")} ${magentaBright("{name}")}       Specify the name of the template
+        ${blueBright("-e")}, ${blueBright("--example")}               Select templates from three.js examples
+        ${blueBright("-f")}, ${blueBright("--force")}                 This will delete all the contents of ${magentaBright("{directory}")}
+        ${blueBright("-b")}, ${blueBright("--bundler" + magentaBright(" {name}"))}        Select the bundler to use ${blueBright("[webpack|parcel]")} (looking for contributors)
+        ${blueBright("--prefer-npm")}                This will prefer npm when installing dependencies
         
     Example:
-        ${chalk.greenBright(
-          "npx create-three-app my-three-app -e -t webgl_shader"
-        )}
+        ${greenBright("npx create-three-app my-three-app -e -t webgl_shader")}
 
     More information:
-        Visit ${chalk.greenBright(
-          "https://github.com/GmBodhi/create-three-app"
-        )}
+        Visit ${greenBright("https://github.com/GmBodhi/create-three-app")}
         `);
   process.exit(0);
 };
