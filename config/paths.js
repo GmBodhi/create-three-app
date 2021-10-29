f1edbe721a971b9bdf6bfcf7bcb3fcdf40765d5b
GmBodhi <71921036+GmBodhi@users.noreply.github.com>
2021-10-29 21:33:33 +0530

    cta

--- config/paths.js
+++ config/paths.js
@@ -18,7 +18,7 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 const publicUrlOrPath = getPublicUrlOrPath(
   process.env.NODE_ENV === 'development',
   require(resolveApp('package.json')).homepage,
-  "create-three-app/"
+  ""
 );
 
 const buildPath = process.env.BUILD_PATH || 'docs';
