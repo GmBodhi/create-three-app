ab62601c117f727ba990e18aa93e0d7ff54760a4
GmBodhi <GmBodhi@users.noreply.github.com>
2021-11-24 00:28:19 +0000

    automated: Examples generated (actions)

---
+++ example-processor/templates/webgl_loader_texture_logluv/src/main.js
@@ -0,0 +1,88 @@
+import "./style.css"; // For webpack support
+
+import {
+  WebGLRenderer,
+  ReinhardToneMapping,
+  sRGBEncoding,
+  Scene,
+  OrthographicCamera,
+  MeshBasicMaterial,
+  PlaneGeometry,
+  Mesh,
+} from "three";
+
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js";
+
+import { LogLuvLoader } from "three/examples/jsm/loaders/LogLuvLoader.js";
+
+const params = {
+  exposure: 2.0,
+};
+
+let renderer, scene, camera;
+
+init();
+
+function init() {
+  renderer = new WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  renderer.toneMapping = ReinhardToneMapping;
+  renderer.toneMappingExposure = params.exposure;
+
+  renderer.outputEncoding = sRGBEncoding;
+
+  scene = new Scene();
+
+  const aspect = window.innerWidth / window.innerHeight;
+
+  camera = new OrthographicCamera(-aspect, aspect, 1, -1, 0, 1);
+
+  new LogLuvLoader().load("textures/memorial.tif", function (texture) {
+    const material = new MeshBasicMaterial({ map: texture });
+
+    const quad = new PlaneGeometry(1, 1.5);
+
+    const mesh = new Mesh(quad, material);
+
+    scene.add(mesh);
+
+    render();
+  });
+
+  //
+
+  const gui = new GUI();
+
+  gui.add(params, "exposure", 0, 4, 0.01).onChange(render);
+  gui.open();
+
+  //
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  const aspect = window.innerWidth / window.innerHeight;
+
+  const frustumHeight = camera.top - camera.bottom;
+
+  camera.left = (-frustumHeight * aspect) / 2;
+  camera.right = (frustumHeight * aspect) / 2;
+
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  render();
+}
+
+//
+
+function render() {
+  renderer.toneMappingExposure = params.exposure;
+
+  renderer.render(scene, camera);
+}
