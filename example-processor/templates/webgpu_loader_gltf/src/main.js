2582186837998a7ff03ef2af20b76b4c67747fa5
GmBodhi <GmBodhi@users.noreply.github.com>
2024-01-05 00:09:28 +0000

    automated: Examples generated (actions)

--- example-processor/templates/webgpu_loader_gltf/src/main.js
+++ example-processor/templates/webgpu_loader_gltf/src/main.js
@@ -9,6 +9,8 @@ import {
 } from "three";
 
 import WebGPU from "three/addons/capabilities/WebGPU.js";
+import WebGL from "three/addons/capabilities/WebGL.js";
+
 import WebGPURenderer from "three/addons/renderers/webgpu/WebGPURenderer.js";
 
 import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
@@ -22,10 +24,10 @@ init();
 render();
 
 function init() {
-  if (WebGPU.isAvailable() === false) {
+  if (WebGPU.isAvailable() === false && WebGL.isWebGL2Available() === false) {
     document.body.appendChild(WebGPU.getErrorMessage());
 
-    throw new Error("No WebGPU support");
+    throw new Error("No WebGPU or WebGL2 support");
   }
 
   const container = document.createElement("div");
